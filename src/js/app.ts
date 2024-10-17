aecd1bf087bab11074db9c126237a9f67579c1e9
Nıcklas Moberg <2671356+nicksen@users.noreply.github.com>
2024-10-17 22:00:14 +0200

    safer script execution

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,6 +1,6 @@
 document.addEventListener(`click`, ({ target }) => {
-	if (target instanceof HTMLElement) {
-		const src = target.getAttribute(`data-play`)!
+	let src
+	if (target instanceof HTMLElement && (src = target.getAttribute(`data-play`))) {
 		const audio = new Audio(src)
 		audio.play()
 	}
