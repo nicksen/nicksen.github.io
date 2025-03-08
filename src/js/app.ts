1b13852d46dc25a65b9b162a56f1c55e44b123aa
Nıcklas Moberg <2671356+nicksen@users.noreply.github.com>
2025-03-08 18:16:16 +0100

    restructure script

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,7 +1,18 @@
-document.addEventListener(`click`, ({ target }) => {
-	let src
-	if (target instanceof HTMLElement && (src = target.getAttribute(`data-play`))) {
-		const audio = new Audio(src)
-		audio.play()
+async function main() {
+	for (const el of document.querySelectorAll(`button`)) {
+		el.addEventListener(`click`, playSound)
 	}
-})
+}
+
+async function playSound(this: HTMLButtonElement, _evt: MouseEvent): Promise<void> {
+	const src = this.value
+	const audio = new Audio(src)
+
+	audio.play()
+}
+
+try {
+	await main()
+} catch (err) {
+	console.error(err)
+}
